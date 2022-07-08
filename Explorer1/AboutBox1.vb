2f61e6e7de5c5604282aa5e3ee48a72c54864077
sebastian2007bro <70060074+sebastian2007bro@users.noreply.github.com>
2022-07-08 13:54:12 +0200

    Add files via upload

---
+++ Explorer1/AboutBox1.vb
@@ -0,0 +1,30 @@
+﻿Public NotInheritable Class AboutBox1
+
+    Private Sub AboutBox1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        
+    End Sub
+
+    Private Sub OKButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OKButton.Click
+        'If Form20.ProgressBar1.Value = 100 Then
+        'Form8.Show()
+        'End If
+        Me.Close()
+        'If Form28.PictureBox1.Visible = True Then
+        'AboutBox20.Show()
+        'End If
+        'If AboutBox19.Label1.Visible = True Then
+        'AboutBox20.Show()
+        'End If
+        'If Form17.Text = "app" Then
+        'Form23.Show()
+        'End If
+    End Sub
+
+    Private Sub TableLayoutPanel_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles TableLayoutPanel.Paint
+
+    End Sub
+
+    Private Sub LabelVersion_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles LabelVersion.Click
+
+    End Sub
+End Class
